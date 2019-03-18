72c9756b0cf2e9d03a2aaba6acded28a000c238f
Vladimir <shikarno_3000@inbox.ru>
2019-03-18 03:05:40 +0300

    main-bg fixed, product-card corner and some small fixes has been added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -10,17 +10,33 @@ function Product(props) {
   // }
   const { id, pretitle, title, taste, amount, bonus, imgUrl } = props.product;
   return (
-    <div className='product' key={id}>
-      <p className='product-pretitle'>{pretitle}</p>
-      <h3 className="product-title">{title}</h3>
-      <h4 className='product-taste'>{taste}</h4>
-      <p className='product-amount'>{amount}</p>
-      <p className='product-bonus'>{bonus}</p>
-      {/* <Link to="/details" style={{ textDecoration: 'none' }} onClick={clickHanler}> */}
-      <img className='product-image' src={imgUrl} alt="" />
+    
+      <label className='product-wrapper'>
+      <input type="checkbox" />
+      <svg width="320" height="480">
+      <g fill='none' stroke='#1698d9'>
+      <foreignObject width="320" height="480">
+      <div className='product' key={id}>
+          <p className='product-pretitle'>{pretitle}</p>
+          <h3 className="product-title">{title}</h3>
+          <h4 className='product-taste'>{taste}</h4>
+          <p className='product-amount'>{amount}</p>
+          <p className='product-bonus'>{bonus}</p>
+          <img className='product-image' src={imgUrl} alt="" />
+        </div>
+      </foreignObject>
+      <polygon class="ddo" points='45,0 320,0 320,480 0,480 0,45' stroke='#1698d9' strokeWidth='5' />
+      </g>
       
-      {/* </Link> */}
-    </div>
+      
+      
+				
+        
+			</svg>
+        
+      </label>
+
+    
   )
 }
 
